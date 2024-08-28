75d1367e74f0389ac2b37e89d94528d4ada4cd03
hakimamarullah <hakimamarullah@gmail.com>
2024-08-28 20:32:05 +0700

    Add OpenAPi

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { ErrorFilter } from './common/exceptions/error.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const logger: Logger = new Logger('ApiWilayahRiApp');
@@ -17,6 +18,17 @@ async function bootstrap() {
   ) as ConfigService;
 
   const port: number = configService.get('SERVER_PORT', 3000);
+  const config = new DocumentBuilder()
+    .setTitle(configService.get<string>('SWAGGER_TITLE', 'API Wilayah RI'))
+    .setDescription(
+      configService.get<string>('SWAGGER_DESCRIPTION', 'API Wilayah RI'),
+    )
+    .setVersion(configService.get<string>('APP_VERSION', '1.0'))
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document, {
+    jsonDocumentUrl: 'swagger/json',
+  });
   app.listen(port).then(() => logger.log(`Server started on port ${port}`));
 }
 
