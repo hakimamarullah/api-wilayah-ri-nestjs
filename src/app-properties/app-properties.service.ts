2b2c4319c87b00e95b0f89c2832bb324fe0eccdd
hakimamarullah <hakimamarullah@gmail.com>
2024-09-16 11:51:21 +0700

    Refactor get auth bearer token props

--- src/app-properties/app-properties.service.ts
+++ src/app-properties/app-properties.service.ts
@@ -20,14 +20,14 @@ export class AppPropertiesService {
   }
 
   public getApiKeyServiceBearerToken(): string {
-    return this.configService.get<string>('API_KEY_MANAGER_BEARER_TOKEN', '');
+    return this.configService.get<string>('AUTH_BEARER_TOKEN', '');
   }
 
   public getAuthServiceBaseUrl() {
-    return this.configService.get('AUTH_SERVICE_URL', 'http://localhost:3001');
+    return this.configService.get('AUTH_BASE_URL', 'http://localhost:3001');
   }
   public getAuthServiceToken() {
-    return this.configService.get('AUTH_SERVICE_TOKEN', 'token');
+    return this.configService.get('AUTH_BEARER_TOKEN', 'token');
   }
 
   public getAuthServiceHttpTimeout() {
